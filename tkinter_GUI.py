817bba9647442e002d6f40655a4a255cc51fa1ca
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2023-10-28 15:03:54 +0530

    pre processing input data

--- tkinter_GUI.py
+++ tkinter_GUI.py
@@ -2,17 +2,48 @@ import tkinter as tk
 from tkinter import messagebox
 from joblib import load
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+def preprocess_input(locality_encoder, input_data):
+    # Convert locality to numerical using the encoder
+    if 'locality' in input_data:
+        input_data['locality'] = locality_encoder.transform([input_data['locality']])[0]
+    
+    # Convert type_bhk to numerical format
+    if 'type_bhk' in input_data:
+        input_data['type_bhk'] = int(input_data['type_bhk'])
+    
+    # Add other preprocessing steps if needed
+    return input_data
 
 def predict():
     try:
         # Load the saved model
         model = load('House_price_prediction.joblib')
-
+        
+        # Load the fitted label encoders
+        with open('label_encoders.pkl', 'rb') as file:
+            encoders = pickle.load(file)
+            locality_encoder = encoders['locality']
+            # Load other encoders if available
+        
         # Get user input values from Entry widgets
-        input_values = [float(entry.get()) for entry in entry_list]
+        input_values = {
+            'bathroom': float(entry_list[0].get()),
+            'floor': float(entry_list[1].get()),
+            'locality': entry_list[2].get(),
+            'property_age': float(entry_list[3].get()),
+            'property_size': float(entry_list[4].get()),
+            'totalFloor': float(entry_list[5].get()),
+            'type_bhk': entry_list[6].get()
+        }
+
+        # Preprocess the input data
+        input_values = preprocess_input(locality_encoder, input_values)
 
         # Use the model to make a prediction
-        predicted_output = model.predict(np.array(input_values).reshape(1, -1))
+        predicted_output = model.predict(np.array(list(input_values.values())).reshape(1, -1))
 
         # Display the predicted output
         predicted_label.config(text=f'Predicted Output: {predicted_output[0]}')
